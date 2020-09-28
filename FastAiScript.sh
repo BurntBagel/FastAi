c872a7cc84b3708ac223778c610f0a0324339544
BurntBagel <67555069+BurntBagel@users.noreply.github.com>
2020-09-28 22:29:49 +0430

    Update FastAiScript.sh

--- FastAiScript.sh
+++ FastAiScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to create Azure DSVM Spot instance with NVidia K80 GPU and install fast.ai library
-read -p "Azure VM Name (default: fastai2): " vminput
-vmname=${vminput:=fastai2}
+read -p "Azure VM Name (default: fastpotato): " vminput
+vmname=${vminput:=fastpotato}
 while [ $password != $password2 ] ; do 
 read -s -p "Choose your Password: " password
 echo
@@ -13,7 +13,7 @@ az group create --name $vmname -l EastUS
 
 echo "Creating Azure Data Science VM $vmname..."
 # You can change the size parameter if you want something other than NVidia K80 GPU instance. 
-# You can find Azure size labels by running Azure CLI command in Cloud Shell "az vm list-sizes -l East US -o table"  
+# You can find Azure size labels by running Azure CLI command in Cloud Shell "az vm list-sizes -l EastUS -o table"  
 az vm create --name $vmname -g $vmname --image microsoft-dsvm:ubuntu-1804:1804:latest  --priority Low --size Standard_NC6 --eviction-policy Deallocate --storage-sku StandardSSD_LRS --admin-user fastuser --admin-password $password
 az vm open-port --name $vmname -g $vmname --port 8000
 echo "Installing  fastai v2 and notebooks..."
